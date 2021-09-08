1506f3dd758f2072e9f25f03646b35624fe23263
jsxgod <smykoo7@gmail.com>
2021-09-08 21:22:41 +0200

    Add text to logo div

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,32 +4,31 @@ const NavBar = () => {
     return (
         <div className="navbar">
             <div className="navbar-inner">
-                <div className="logo">   
+                <div className="logo">CMPN.</div>
+                <nav className="nav">
+                    <li>
+                        <a href="/design">Design</a>
+                    </li>
+                    <li>
+                        <a href="/strategy">Strategy</a>
+                    </li>
+                    <li>
+                        <a href="/cases">Cases</a>
+                    </li>
+                    <li>
+                        <a href="/about">About</a>
+                    </li>
+                    <li>
+                        <a href="/why">Why work with us?</a>
+                    </li>
+                </nav>
+                <div className="contact">
+                    <a href="/contact">Let's work together</a>
                 </div>
-                    <nav className="nav">
-                        <li>
-                            <a href="/design">Design</a>
-                        </li>
-                        <li>
-                            <a href="/strategy">Strategy</a>
-                        </li>
-                        <li>
-                            <a href="/cases">Cases</a>
-                        </li>
-                        <li>
-                            <a href="/about">About</a>
-                        </li>
-                        <li>
-                            <a href="/why">Why work with us?</a>
-                        </li>
-                    </nav>
-                    <div className="contact">
-                        <a href="/contact">Let's work together</a>
-                    </div>
-                    <div className="menu">
-                        <span></span>
-                        <span></span>
-                    </div>
+                <div className="menu">
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
         </div>
     );
