102239faf871351babd9fe680b44fd7bffb0733c
jsxgod <smykoo7@gmail.com>
2021-09-09 22:25:35 +0200

    Clean up exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { default as NavBar } from './NavBar';
 export { default as Banner } from './Banner';
+export { default as Image } from './Image';
+export { default as ImageBlock } from './ImageBlock';
+export { default as Loader } from './Loader';
