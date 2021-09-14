355b61717bb8caab13fb0a90f6ef853c526ce373
jsxgod <smykoo7@gmail.com>
2021-09-14 15:54:56 +0200

    Clean up exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as NavBar } from './NavBar';
+export { default as Menu } from './Menu';
 export { default as Banner } from './Banner';
 export { default as Image } from './Image';
 export { default as ImageBlock } from './ImageBlock';
