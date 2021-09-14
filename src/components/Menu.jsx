09528256e7499491254a9dc745603499045b2703
jsxgod <smykoo7@gmail.com>
2021-09-14 17:16:03 +0200

    Clean up animation code for menu items. Stagger menu items' animations

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,37 +3,37 @@ import { motion } from "framer-motion";
 
 const Menu = ({ opened }) => {
   const variants = {
-    hide: {
-      width: "0px",
-      transition: {
-        ease: [0.6, 0.01, -0.05, 0.95],
-        duration: 1,
-      },
-    },
     show: {
       width: "100vw",
       transition: {
         ease: [0.6, 0.01, -0.05, 0.95],
         duration: 1,
+        staggerChildren: 0.1,
+        delayChildren: 0.4,
+      },
+    },
+    hide: {
+      width: "0px",
+      transition: {
+        ease: [0.6, 0.01, -0.05, 0.95],
+        duration: 0.8,
       },
     },
   };
 
   const menu_item_variants = {
-    hide: {
-      opacity: 0,
-      y: "0",
-      transition: {
-        ease: "easeInOut",
-        duration: 0.4,
-      },
-    },
     show: {
       opacity: 1,
-      y: "0",
       transition: {
         ease: "easeInOut",
         duration: 0.5,
+      },
+    },
+    hide: {
+      opacity: 0,
+      transition: {
+        ease: "easeInOut",
+        duration: 0.3,
       },
     },
   };
@@ -44,63 +44,30 @@ const Menu = ({ opened }) => {
       variants={variants}
       animate={opened ? "show" : "hide"}
     >
-      {console.log(opened)}
       <div className="menu-inner">
         <nav className="menu-nav">
           <li>
-            <motion.a
-              href="/design"
-              variants={menu_item_variants}
-              animate={opened ? "show" : "hide"}
-              transition={{
-                delay: 0.2,
-              }}
-            >
+            <motion.a href="/design" variants={menu_item_variants}>
               Design
             </motion.a>
           </li>
           <li>
-            <motion.a
-              href="/strategy"
-              variants={menu_item_variants}
-              animate={opened ? "show" : "hide"}
-              transition={{
-                delay: 0.15,
-              }}
-            >
+            <motion.a href="/strategy" variants={menu_item_variants}>
               Strategy
             </motion.a>
           </li>
           <li>
-            <motion.a
-              href="/cases"
-              variants={menu_item_variants}
-              animate={opened ? "show" : "hide"}
-              transition={{
-                delay: 0.1,
-              }}
-            >
+            <motion.a href="/cases" variants={menu_item_variants}>
               Cases
             </motion.a>
           </li>
           <li>
-            <motion.a
-              href="/about"
-              variants={menu_item_variants}
-              animate={opened ? "show" : "hide"}
-              transition={{
-                delay: 0.05,
-              }}
-            >
+            <motion.a href="/about" variants={menu_item_variants}>
               About
             </motion.a>
           </li>
           <li>
-            <motion.a
-              href="/why"
-              variants={menu_item_variants}
-              animate={opened ? "show" : "hide"}
-            >
+            <motion.a href="/why" variants={menu_item_variants}>
               Why work with us?
             </motion.a>
           </li>
